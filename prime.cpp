3e5f33221ea10ad5b8c5a7f0fccac6bef8669998
ninadthora09 <ninadthorat4@gmail.com>
2025-06-24 14:53:29 +0530

    Done

---
+++ prime.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string primeChecker(int num){    // #1
+    int count;
+    for(int i=1;i<=num;i++){
+        if(num%i==0){
+            count++;
+        }
+    }
+    if(count==2) return "Yes Prime";
+    else return "Not a Prime";
+}
+
+int primeInRangeCount(int m){    // #2
+    int mainCount=0;
+    for(int i=m;i>=1;i--){
+        int count =0;
+        for(int j=m;j>=1;j--){
+            if(i%j==0){
+                count++;
+            }
+        }
+        if(count==2){
+            mainCount++;
+        }
+    }
+    return mainCount;
+}
