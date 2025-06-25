2fc3cf82d0666acba6c262a35153fe3152c16f90
ninadthora09 <ninadthorat4@gmail.com>
2025-06-25 19:18:55 +0530

    Done

---
+++ PrimeFactorOfNum.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// PrimeFactor of a Number
+vector<int> PrimeFactor(int num)
+{
+    vector<int> arr;
+    auto PrimeNum = [](int n) { // lambda function used in c++
+        int count = 0;
+        int value;
+        for (int i = n; i >= 2; i--)
+        {
+            for (int j = i; j >= 1; j--)
+            {
+                if (i % j == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                value = i;
+            }
+            else
+                return 0;
+            break;
+        }
+        return value;
+    };
+    for (int i = num; i >= 3; i--)
+    {
+        if (num % (i - 1) == 0)
+        {
+            int result = PrimeNum(i - 1);
+            if (result == 0)
+            {
+                continue;
+            }
+            else
+                arr.push_back(result);
+        }
+        else
+            continue;
+    }
+    return {arr};
+}
