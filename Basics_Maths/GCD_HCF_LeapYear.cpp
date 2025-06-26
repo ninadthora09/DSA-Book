5404b211af7faa2945864a8ef0a558ba29b2e167
ninadthora09 <ninadthorat4@gmail.com>
2025-06-26 22:21:42 +0530

    Final

--- Basics_Maths/GCD_HCF_LeapYear.cpp
+++ Basics_Maths/GCD_HCF_LeapYear.cpp
@@ -14,3 +14,12 @@ int gcdNum(int n, int m)
     }
     return 1;
 }
+
+// leap year code
+void Leap_year(int year){
+    if((year%4==0 and year%100!=0) /*This covers normal leap years like 2024, 2016, etc.*/or (year%400==0)/*Like 2000, 1600)*/)
+    {
+        cout<<"Leap year";
+    }
+    else cout<<"Not a leap year";
+}
