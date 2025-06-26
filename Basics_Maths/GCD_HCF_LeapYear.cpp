46a61cbf6d7adf310978957f0aea40824e222c60
ninadthora09 <ninadthorat4@gmail.com>
2025-06-26 23:36:07 +0530

    new

--- Basics_Maths/GCD_HCF_LeapYear.cpp
+++ Basics_Maths/GCD_HCF_LeapYear.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int gcdNum(int n, int m)
-{
+{ // gcd and hcf are same (greatest common divisor and higest common factor)
     if (n == 0 or m == 0)
         return 0;
     n = abs(n);
@@ -15,11 +15,42 @@ int gcdNum(int n, int m)
     return 1;
 }
 
-// leap year code
-void Leap_year(int year){
-    if((year%4==0 and year%100!=0) /*This covers normal leap years like 2024, 2016, etc.*/or (year%400==0)/*Like 2000, 1600)*/)
+int lcmNum(int num1, int num2)
+{
+    // LCM mean lowest common (multiple)
+
+    // #1 dircect formula
+    int lcm = (num1 * num2) / gcdNum(num1, num2);
+    // return lcm;
+
+    // #2 is direct method
+    num1 = abs(num1);
+    num2 = abs(num2);
+    int mulOfbothNum = num1 * num2;
+    for (int i = 1; i <= mulOfbothNum; i++)
     {
-        cout<<"Leap year";
+        if (i % num1 == 0 and i % num2 == 0)
+        {
+            return i;
+        }
     }
-    else cout<<"Not a leap year";
+    return 0;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int final = lcmNum(a, b);
+    cout << final;
+}
+
+// leap year code
+void Leap_year(int year)
+{
+    if ((year % 4 == 0 and year % 100 != 0) /*This covers normal leap years like 2024, 2016, etc.*/ or (year % 400 == 0) /*Like 2000, 1600)*/)
+    {
+        cout << "Leap year";
+    }
+    else
+        cout << "Not a leap year";
 }
